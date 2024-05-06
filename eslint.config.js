7d2ae5c9a1c52384128b1d8ba9be82a163b5c1b8
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-05-05 23:23:00 -0700

    config eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import js from "@eslint/js";
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReactConfig,
+  js.configs.recommended,
+
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+    }
+  }
 ];
